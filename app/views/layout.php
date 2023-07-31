8d6d0599053795c72b98d48c0357b5fcf881a294
Misha Kozyr <kozyrmisha@icloud.com>
2023-07-31 20:55:33 +0300

    Add layout for View class

---
+++ app/views/layout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $title ?? 'My App' ?></title>
+</head>
+<body>
+    <div id="header">
+        <!-- Header content here -->aaa
+    </div>
+    <div id="content">
+        <?= $content ?>
+    </div>
+    <div id="footer">
+        <!-- Footer content here -->aaa
+    </div>
+</body>
+</html>
--- app/views/main.php
+++ app/views/main.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
-</head>
-<body>
-    <h1>Main page</h1>
-    <div>
-        <a href="/login">Войти</a>
-        <a href="/register">Зарегистрироваться</a>
-    </div>
-</body>
-</html>
+<h1>Main page</h1>
+<div>
+    <a href="/login">Войти</a>
+    <a href="/register">Зарегистрироваться</a>
+</div>
